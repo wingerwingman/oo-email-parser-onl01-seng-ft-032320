499769f4042f03634ccdea0e35a2be3a6e5017e9
Justin Winger <justinwinger4@gmail.com>
2020-04-02 14:55:43 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,7 @@
 # Build a class EmailParser that accepts a string of unformatted 
+class EmailParser
+  
+end
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
