320d623a9610e897415621079dc4a9872821058e
Justin Winger <justinwinger4@gmail.com>
2020-04-02 15:00:49 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,6 +1,8 @@
 # Build a class EmailParser that accepts a string of unformatted 
 class EmailAddressParser
   attr_accessor :emails
+  
+  emails = []
   def EmailParse(emails)
     emails.split(" ")
   end
